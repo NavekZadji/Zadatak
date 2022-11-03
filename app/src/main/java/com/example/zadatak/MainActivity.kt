8ad552c14bab44e38cdf4ac4ed93f956d9a54388
NavekZadji <brunno.hrastic@gmail.com>
2022-11-03 22:40:17 +0100

    Probni ViewPager2

--- app/src/main/java/com/example/zadatak/MainActivity.kt
+++ app/src/main/java/com/example/zadatak/MainActivity.kt
@@ -3,19 +3,35 @@ package com.example.zadatak
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.component.KoinComponent
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(), KoinComponent {
+
     private val viewModel by viewModel<NewsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycleScope.launch {
-            val result = viewModel.getNews()
-            findViewById<TextView>(R.id.tekst).text = result.body()
-        }
+
+        val data = arrayOf("aa", "bb", "cc", "dd")
+        pager.adapter = ViewPagerAdapter(data)
+        // prepoznal je xml objekt tek dok sam dodal v gradle apply plugin: 'com.android.application'  apply plugin: 'kotlin-android'  apply plugin: 'kotlin-android-extensions'
+        // i jos v MainActivity import kotlinx.android.synthetic.main.activity_main.*
+        pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                Toast.makeText(this@MainActivity, "onPageSelected", Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 }
